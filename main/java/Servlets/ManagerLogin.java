7129d698d9c06aa98b175979d1b98c24c26576e0
Daniel Vais <daniel928@revature.net>
2022-05-31 15:36:57 -0700

    test

---
+++ main/java/Servlets/ManagerLogin.java
@@ -0,0 +1,31 @@
+package Servlets;
+
+import EmployeeInfo.Employee;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ManagerLogin extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        request.getRequestDispatcher("nav.html").include(request, response);
+
+        String username = request.getParameter("username");
+
+        Employee employee = new Employee();
+        employee.setUsername(username);
+        employee.setPassword(request.getParameter("password"));
+
+        Cookie cookie = new Cookie("username", username);
+        response.addCookie(cookie);
+
+    }
+}
---
+++ main/java/Servlets/ManagerProfile.java
@@ -0,0 +1,25 @@
+package Servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ManagerProfile extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        request.getRequestDispatcher("nav.html").include(request, response);
+        request.getRequestDispatcher("managerprofile.html").include(request, response);
+
+        Cookie[] cookies = request.getCookies();
+        String username = cookies[0].getValue();
+
+        HttpSession session = request.getSession();
+        session.setAttribute("username", username);
+
+        out.close();
+    }
+}
